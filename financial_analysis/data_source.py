d26d7260721b4664acf5ba30b54679e13cea4e03
envisocy <envisocy@gmail.com>
2018-01-06 20:12:34 +0800

    01-06-2018

--- financial_analysis/data_source.py
+++ financial_analysis/data_source.py
@@ -4,19 +4,23 @@
 import numpy as np
 import requests
 import configure
+from financial_analysis.config import *
 
 class caibaoshuo():
     '''
-    数据来源：caibaoshuo.cn API
+    caibaoshuo.cn API 2018
     '''
+    import numpy as np
+    import pandas as pd
 
-    def __init__(self, api_name="caibaoshuo"):
-        '''
-        在 caibaoshuo.cn 稳定情况下，不需要传递任何参数
-        '''
-        self._api_token = configure.echo(api_name)["config"]["api"]
+    def __init__(self, data_sources="caibaoshuo"):
+        import configure
+        if data_sources=="caibaoshuo":
+            self._api_token = configure.echo(data_sources)["config"]["api"]
+        else:
+            self.sql = configure.echo(data_sources)["config"]
 
-    def request(self, sheets: str="mj", index_key: str="000001"):
+    def get_data(self, sheets="mj", code="000001"):
         '''
         Parameter:
         - sheets(str):
@@ -25,25 +29,72 @@ class caibaoshuo():
             - bs: balance_sheets 资产负债表
             - pl: income_statements 利润表
             - cf: cash_flow_statements 现金流量表
-        - index_key(str):
+        - code(str):
             - 000000: 全部公司信息
             - 000001: 例：中国平安
         '''
-        part = {"c": "companies","cp": "companies", "company_information": "companies",
-                "f": "mjanalyses", "mj": "mjanalyses", "financial_analysis_table": "mjanalyses",
-                "b": "bssheets", "bs": "bssheets", "balance_sheets": "bssheets",
-                "i": "plsheets", "pl": "plsheets", "income_statements": "plsheets",
-                "c": "cfsheets", "cf": "cfsheets", "cash_flow_statements": "cfsheets"
+        import requests
+        # "cp" 为1.0版本的接口，1.1版本沿用
+        # "oj" 为1.0版本的接口
+        # "mj" 为1.1版本的接口，添加了两个变量，但未测试通过
+        # "bl","ic","cf" 为1.0版本接口，不知道会不会弃用
+        part = {"cp": "companies", "company_information": "companies",
+                "oj": "mjanalyses",
+                "mj": "genealsheets", "financial_analysis_table": "genealsheets",
+                "bl": "bssheets", "balance_sheets": "bssheets",
+                "ic": "plsheets", "income_statements": "plsheets",
+                "cf": "cfsheets", "cash_flow_statements": "cfsheets"
                }
         if sheets not in part:
             sheets = "mj"
-            index_key="000001"
-        if part[sheets] == "companies" and index_key == "000000":
-            url = "http://caibaoshuo.com/api/v1/companies?auth_token=" +\
-            self._api_token
+            code="000001"
+        if part[sheets] == "companies" and code == "000000":
+            url = "http://caibaoshuo.com/api/v1/companies?auth_token=" + self._api_token
         else:
-            url = "http://caibaoshuo.com/api/v1/" + part[sheets] + "/" +\
-            index_key + "?auth_token=" + self._api_token
+            url = "http://caibaoshuo.com/api/v1/" + part[sheets] + "/" + code + "?auth_token=" + self._api_token
         # requests
         response = requests.get(url)
-        return eval(response.text.replace("null", "np.nan"))
+        if response.status_code == 200:
+            return eval(response.text.replace("null", "np.nan"))['data']
+        else:
+            print(response.status_code)
+            return None
+
+    def industry_list(self, industry="互联网", companies=""):
+        if not companies:
+            companies = self.get_data("cp", "000000")
+        df = pd.DataFrame(companies)
+        return df.loc[df["industry"] == industry, "code"].tolist()
+
+    def request_list(self, code="000002"):
+        companies = self.get_data(sheets="cp", code="000000")
+        df = pd.DataFrame(companies)
+        industry = df.loc[df["code"] == code,"industry"].iloc[0]
+        return self.industry_list(industry=industry, companies=companies)
+
+    def list_to_df(self, code_list, sheets="mj", variable="grossMargin_ratio"):
+        import time
+        df = pd.DataFrame()
+        for code in code_list:
+            print(" * 请求:" + code + "中...(共" + str(code_list.index(code) + 1) + "/" + str(len(code_list)) +"项)")
+            df_code = pd.DataFrame(self.get_data(sheets=sheets, code=code))
+            if not len(df_code):
+                continue
+            df_code = df_code.loc[:,["end_year", variable]]
+            df_code["code"] = code
+            df = pd.concat([df,df_code],axis=0)
+            time.sleep(5)
+        return df
+
+    def swarmplot(self, code="000002", sheets="mj", variable="grossMargin_ratio"):
+        variable_name = caibaoshuo_geneal_sheets_columns.get(variable, variable)
+        print(" * 请求行业数据 ...")
+        industry_list = self.request_list(code=code)
+        print(" * 得到行业列表 ...")
+        df = self.list_to_df(code_list=industry_list, sheets=sheets, variable=variable)
+        print("绘图中")
+        df.rename(columns={"end_year":"年报年份", variable:variable_name}, inplace=True)
+        df[variable_name + "分布"] = "行业状况"
+        df.loc[df["code"]==code,variable_name+"分布"]=code
+        sns.factorplot(data=df, x="年报年份", y=variable_name, kind="swarm", hue=variable_name+"分布", size=6, aspect=2)
+        return df
