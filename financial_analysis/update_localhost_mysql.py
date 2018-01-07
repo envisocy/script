6acdea6fac84a360e6ca39e49b19c82e6bdb4e36
envisocy <envisocy@gmail.com>
2018-01-07 17:56:44 +0800

    01-07-2018_3

--- financial_analysis/update_localhost_mysql.py
+++ financial_analysis/update_localhost_mysql.py
@@ -56,8 +56,25 @@ def write_to_mysql(TableName, dic):
     cursor.close()
     conn.close()
 
+def return_exist_list():
+    conn = pymysql.connect(host="localhost", user="root", passwd="123456", db="financial_statements")
+    cursor=conn.cursor()
+    cursor.execute("SELECT DISTINCT(`code`) FROM record;")
+    exist_code_list = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+    exist_code_list = [i[0] for i in exist_code_list]
+    return exist_code_list
+
 cbs = financial_analysis.data_source.caibaoshuo()
 code_list = cbs.request_list(code="")
+print(" - Total: " + str(len(code_list)))
+exist_code_list = return_exist_list()
+print(" - Exist: " + str(len(exist_code_list)))
+for i in exist_code_list:
+    code_list.remove(i)
+print(" - Remaining: " + str(len(code_list)))
 for code in code_list:
     data = cbs.get_data(sheets="mj", code=code)
     print(code + " - " + datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S %f"))
