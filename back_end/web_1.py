c8a277de39b27258a1238e931dfec7660d627202
envisocy <envisocy@gmail.com>
2018-01-24 18:12:11 +0800

    01-24-2018

--- back_end/web_1.py
+++ back_end/web_1.py
@@ -1,7 +1,7 @@
 #!usr/bin/env python
-# -*- coding:utf-8 -*-
 
 import socket
+
 
 def client(host='g.cn',port=80):
     s = socket.socket()
@@ -70,6 +70,18 @@ def test_parsed_url():
         e = "parsed_url ERROR, ({}) ({}) ({})".format(url, u, expected)
         assert u == expected, e
 
+def test_parsed_response():
+    response = 'HTTP/1.1 301 Moved Permanently\r\n' \
+        'Content-Type: text/html\r\n' \
+        'Location: https://movie.douban.com/top250\r\n' \
+        'Content-Length: 178\r\n\r\n' \
+        'test body'
+    response = response.encode('utf-8')
+    status_code, header, body = parsed_response(response)
+    assert status_code == 301
+    assert len(list(header.keys())) == 3
+    assert body == 'test body'
+
 def parsed_url(url):
     """
     解析 url 并返回 (protocol, host, port, path)
@@ -103,3 +115,57 @@ def parsed_url(url):
         port = int(h[1])
 
     return protocol, host, port, path
+
+def parsed_request(path, host):
+    http_request = 'GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n'.format(path, host)
+    request = http_request.encode('utf-8')
+    return request
+
+def response_by_socket(s, buffer_size=1024):
+    response = b''
+    while True:
+        r = s.recv(buffer_size)
+        response += r
+        if len(r) < buffer_size:
+            break
+    return response
+
+def parsed_response(response):
+    r = response.decode('utf-8')
+    header, body = r.split("\r\n\r\n", 1)
+    h = header.split('\r\n')
+    # GET 200 ok
+    status_code = int(h[0].split()[1])
+
+    headers = {}
+    for line in h[1:]:
+        k, v = line.split(': ')
+        headers[k] = v
+
+    return status_code, headers, body
+
+def get(url):
+    protocol, host, port, path = parsed_url(url)
+    if protocol == "http":
+        s = socket.socket()
+    elif protocol == "https":
+        import ssl
+        s = ssl.wrap_socket(socket.socket())
+
+    s.connect((host, port))
+
+    local_ip, local_port = s.getsockname()
+    print('本机 ip 和 port：{}:{}'.format(local_ip, local_port))
+
+    s.send(parsed_request(path, host))
+
+    response = response_by_socket(s)
+
+    status_code, headers, body = parsed_response(response)
+
+    print(status_code, headers, body)
+    if status_code in [301, 302]:
+        url = headers['Location']
+        return get(url)
+
+    return status_code, headers, body
