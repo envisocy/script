ac2706d4f9966c7a987bb214e7f0761c9539d5b4
envisocy <envisocy@gmail.com>
2018-02-23 16:12:47 +0800

    02-23-2018 Basic Web_test Complete!

--- web_test/routes.py
+++ web_test/routes.py
@@ -24,12 +24,32 @@ def route_static(request):
         img = header.encode("utf-8") + b'\r\n' + f.read()
         return img
 
+def current_user(request):
+    # session_id = request.cookies.get('user', '')
+    # username = session.get(session_id, '【游客】')
+    username = request.cookies.get("user", "【游客】")
+    return username
+
+def response_with_header(headers):
+    header = "HTTP/1.1 201 Test OK\r\n"
+    header += ''.join(['{}: {}\r\n'.format(k, v) for k,v in headers.items()])
+    return header
+
 def route_index(request):
     '''
     主页返回
     '''
-    header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"
+    headers = {
+        'Content-Type': 'text/html',
+    }
+    username = current_user(request)
     body = template("index.html")
+    body = body.replace("{{username}}", username)
+    if username == "【游客】":
+        body = body.replace('{{replace}}', '<a href="/login">【点击登陆】</a><a href="/register">【点击注册】</a>')
+    else:
+        body = body.replace('{{replace}}', '<a href="/messages">【消息测试】</a>')
+    header = response_with_header(headers)
     r = header + "\r\n" + body
     return r.encode("utf-8")
 
@@ -43,11 +63,15 @@ def route_favicon(request):
         return img
 
 def route_login(request):
-    header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"
+    headers = {
+        'Content-Type': 'text/html',
+    }
+    username = current_user(request)
     if request.method == "POST":
-        form = request.form()   # 将内容转化为字典
-        u = User.new(form)      # ?
+        form = request.form()   # 将body内容转化为字典
+        u = User.new(form)
         if u.validate_login():
+            headers["Set-cookie"] = "user={}".format(u.username)
             result = "登录成功，欢迎" + u.username + "回来！"
         else:
             result = "用户名或密码错误！"
@@ -55,11 +79,16 @@ def route_login(request):
         result = ""
     body = template("login.html")
     body = body.replace("{{result}}", result)
+    body = body.replace("{{username}}", username)
+    header = response_with_header(headers)
+    log(body)
     r = header + "\r\n" + body
     return r.encode("utf-8")
 
 def route_register(request):
-    header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"
+    headers = {
+        "Content-Type": "text/html"
+    }
     if request.method == "POST":
         form = request.form()
         u = User.new(form)
@@ -72,21 +101,24 @@ def route_register(request):
         result = ""
     body = template("register.html")
     body = body.replace('{{result}}', result)
+    header = response_with_header(headers)
     r = header + '\r\n' + body
     return r.encode(encoding='utf-8')
 
 def route_message(request):
-    header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"
+    headers = {
+        'Content': 'text/html'
+    }
     log("本次请求的 method", request.method)
     if request.method == "POST":
         form = request.form()
-        log("****", form)
         msg = Message.new(form)
-        log("post", form)
         message_list.append(msg)
     body = template("html_basic.html")
     megs = '<br>'.join([str(m) for m in message_list])
+    body = body.replace("{{username}}", request.cookies.get("user", "【游客】"))
     body = body.replace("{{messages}}", megs)
+    header = response_with_header(headers)
     r = header + "\r\n" + body
     return r.encode(encoding="utf-8")
 
--- web_test/server.py
+++ web_test/server.py
@@ -14,16 +14,17 @@ class Request():
         self.path = "/"
         self.query = {}
         self.body = ""
+        # L.4 cookies 拓展
+        self.headers = {}
+        self.cookies = {}
 
     def log(self):
         '''
         打印所有当前 request 类中的参数值
         '''
-        log("--- Requst Parameter ---")
-        log("Method: ", self.method)
-        log("Path: ", self.path)
-        log("Query: ", self.query)
-        log("Body: ", self.body)
+        pars = "\n".join([" > {}: {}".format(k, v) for k, v in self.
+                          __dict__.items()])
+        log("--- Requst Parameter ---\n" + pars)
 
     def form(self):
         '''
@@ -37,6 +38,20 @@ class Request():
             k, v = arg.split("=")
             f[urllib.parse.unquote(k)] = urllib.parse.unquote(v)
         return f
+
+    def add_headers(self, header):
+        # 更新 headers 的同时更新 cookies
+        for line in header:
+            k, v = line.split(": ", 1)
+            self.headers[k] = v
+        # 更新 cookie
+        self.cookies = {}
+        cookies = self.headers.get("Cookie", "")
+        kvs = cookies.split('; ')
+        for kv in kvs:
+            if '=' in kv:
+                k, v = kv.split('=')
+                self.cookies[k] = v
 
 request = Request()
 
@@ -58,7 +73,7 @@ def run(host="", port=3001):
                 if len(r) < 2048:
                     break
             r = r.decode("utf-8")
-            log("发出的信息：\n", r)
+            # log("发出的信息：\n", r)
             # GET / HTTP/1.1\r\nHost: localhost:3001\r\n
             # Connection: keep-alive...\r\n
 
@@ -68,13 +83,14 @@ def run(host="", port=3001):
             # 特别的，这里在每次运行，都会重新对 request 对象进行赋值覆盖
             request.method = r.split()[0]
             path = r.split()[1]     # 未转化的版本，包含 path和 query
+            request.add_headers(r.split("\r\n\r\n", 1)[0].
+                                          split("\r\n")[1:])
             request.body = r.split("\r\n\r\n", 1)[1]
             request.path, request.query = parsed_path(path) # 函数转化
             request.log()       # 打印 request 的分类信息
 
             # 调用路由，运行响应函数
             response = response_for_path(request.path)
-            print("\n")
             log("--- Retuen Response ---\n", response[:100])
             connection.sendall(response)
             connection.close()
